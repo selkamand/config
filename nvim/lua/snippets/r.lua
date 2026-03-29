f1ebcb42463f3f04659ad7e7f0db6b92cfe54b07
selkamand <sam.elkamand@gmail.com>
2026-03-29 19:48:12 +1100

    feat: roxygen comment snippet

---
+++ nvim/lua/snippets/r.lua
@@ -0,0 +1,31 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local i = ls.insert_node
+local t = ls.text_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+  s(
+    "roxy",
+    fmt(
+      [[
+#' {}
+#' 
+#' {}
+#' 
+#' @return {}
+#' 
+#' @examples
+#' {}
+#' 
+#' @export
+]],
+      {
+        i(1, "title"),
+        i(2, "description"),
+        i(3, "return"),
+        i(4, "example"),
+      }
+    )
+  ),
+}
